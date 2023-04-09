9c898de9b093285364ad0e169af3ee2cd1860010
x1aY <752550308@qq.com>
2023-04-09 11:04:29 +0800

    单调栈 BST

---
+++ src/java/leetcode/LeetcodeHot100/AB_sec50/K_maxRec_85.java
@@ -0,0 +1,24 @@
+package leetcode.LeetcodeHot100.AB_sec50;
+
+public class K_maxRec_85 {
+
+    public static void main(String[] args) {
+        K_maxRec_85 ans = new K_maxRec_85();
+        char[][] mtx = new char[][] {   { '1', '0', '1', '0', '0' }, 
+                                        { '1', '0', '1', '1', '1' },
+                                        { '1', '1', '1', '1', '1' }, 
+                                        { '1', '0', '0', '1', '0' } };
+        ans.maximalRectangle(mtx);
+    }
+
+    public int maximalRectangle(char[][] matrix) {
+        char tgt = '1';
+        
+
+
+
+
+        return 0;
+    }
+
+}
