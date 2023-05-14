c1277713d37746e47941a9adc9e8aa90597a4fc6
x1aY <752550308@qq.com>
2023-05-14 21:16:08 +0800

    字典树，倒排索引

---
+++ src/java/leetcode/LeetcodeHot100/AD_fourth50/I_respace_1713.java
@@ -0,0 +1,12 @@
+package leetcode.LeetcodeHot100.AD_fourth50;
+
+public class I_respace_1713 {
+    
+
+    // https://blog.csdn.net/m0_46202073/article/details/107253959
+    // https://leetcode.cn/problems/re-space-lcci/solution/jian-dan-dp-trieshu-bi-xu-miao-dong-by-sweetiee/
+    public int respace(String[] dictionary, String sentence) {
+        return 0;
+    }
+
+}
